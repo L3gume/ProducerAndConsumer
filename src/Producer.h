e20de87f8e8ba73a57f3217d7da92cde9e1fd5e6
MrL3gume <justin.tremblay@mail.mcgill.ca>
2017-12-04 15:02:07 -0500

    Started work on producer code on feature-producer branch

--- src/Producer.h
+++ src/Producer.h
@@ -3,6 +3,9 @@
 
 struct producer {
 
+    /* These two functions are needed for the producer/consumer structure  */
+    void (*produce)(void);
+    void (*wait)(void);
 };
 
 extern const struct producer Producer;
