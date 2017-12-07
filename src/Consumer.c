24d60a5f97f57e8137e89f95917430363665f3d6
aidssmcc <aidan.sullivan25@gmail.com>
2017-12-06 22:39:56 -0500

    Commenting

--- src/Consumer.c
+++ src/Consumer.c
@@ -12,19 +12,20 @@ void consumer(void) {
     while ((TURN = fopen("../data/turn.txt", "r+")) == NULL);
         //can't open file until producer is done using it
 
-    if('0' != fgetc(TURN)) {	//if turn is not 0
+    if('0' != fgetc(TURN)) {	//if TURN is not 0
         //turn is 1, consumer's turn to read data
         DATA = fopen("../data/data.txt", "r");
-        from_file = fgetc(DATA);
-        putc(from_file, stdout);
+        from_file = fgetc(DATA);    //read next char
+        putc(from_file, stdout);    //print it to screen
         fclose(DATA);
-        rewind(TURN);
-        fputc('0', TURN);
+        rewind(TURN);               //rewind TURN so it can be read again
+        fputc('0', TURN);       //update TURN
     }
 
     fclose(TURN);
 }
 
+//this is used to make the function look like it's part of a library
 const struct consumer_t Consumer = {
     .consumer = consumer
 };
